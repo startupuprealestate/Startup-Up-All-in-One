41bc5b3edde75fb0ed510723c81b6a404ce2164c
startupuprealestate <startup.up.real.estate@gmail.com>
2026-03-30 15:32:05 +0700

    Update trigger.js

--- api/trigger.js
+++ api/trigger.js
@@ -1,13 +1,9 @@
 import admin from 'firebase-admin';
 
-// 💡 1. ยืนยันตัวตนกับ Firebase (ดึงรหัสจาก Vercel Environment)
 if (!admin.apps.length) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
     admin.initializeApp({
-        credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n').replace(/"/g, ''),
-        }),
+        credential: admin.credential.cert(serviceAccount)
     });
 }
 
