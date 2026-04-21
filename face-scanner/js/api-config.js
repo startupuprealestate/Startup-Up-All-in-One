49bc10297c70f935563b6694ec330fcc552ef82f
startupuprealestate <startup.up.real.estate@gmail.com>
2026-04-21 11:42:44 +0700

    Update api-config.js

--- face-scanner/js/api-config.js
+++ face-scanner/js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzZ2jG2o8E1l23qwDGeufGmWjxn74D-AFuEfwQ67F2a7pLe0KIGBkvDoM7NbKlJXMPxkA/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycby1PaRT8l97SL10Z-srj_ibGFA0ODt7V_T1cof3HfjF1MU3282SprrNsDqUSruTltp7zw/exec';
